cde47e2b02230787e52967d4f5a791a3b13fd4fb
Ron Ward <defroged@gmail.com>
2024-05-18 09:29:29 +0900

    mobile panorama

--- js/panorama.js
+++ js/panorama.js
@@ -4,21 +4,27 @@ document.addEventListener("DOMContentLoaded", function() {
   const img2 = document.getElementById("img2");
 
   function adjustImagePositions(newLeft) {
+    const imgWidth = img1.offsetWidth;
     if (newLeft > 0) {
-      img1.style.left = `${newLeft - img1.offsetWidth}px`;
+      img1.style.left = `${newLeft - imgWidth}px`;
       img2.style.left = `${newLeft}px`;
-    } else if (newLeft < -img1.offsetWidth) {
-      img1.style.left = `${newLeft + img2.offsetWidth}px`;
+    } else if (newLeft < -imgWidth) {
+      img1.style.left = `${newLeft + imgWidth}px`;
       img2.style.left = `${newLeft}px`;
     } else {
       img1.style.left = `${newLeft}px`;
-      img2.style.left = `${newLeft + img1.offsetWidth}px`;
+      img2.style.left = `${newLeft + imgWidth}px`;
     }
   }
 
-  const initialPosition = -img1.offsetWidth / 3;
-  adjustImagePositions(initialPosition);
+  function updateInitialPosition() {
+    const imgWidth = img1.offsetWidth;
+    const initialPosition = -imgWidth / 3;
+    adjustImagePositions(initialPosition);
+    return initialPosition;
+  }
 
+  let initialPosition = updateInitialPosition();
   let lastScrollY = window.pageYOffset;
   let targetLeft = initialPosition;
   let currentLeft = initialPosition;
@@ -30,15 +36,22 @@ document.addEventListener("DOMContentLoaded", function() {
   function handleScroll() {
     const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     const scrollPercentage = window.pageYOffset / scrollHeight;
-    const startPosition = -img1.offsetWidth / 3;
-    targetLeft = startPosition + (-img1.offsetWidth * scrollPercentage);
+    const imgWidth = img1.offsetWidth;
+    const startPosition = -imgWidth / 3;
+    targetLeft = startPosition + (-imgWidth * scrollPercentage);
   }
 
   function animate() {
-    currentLeft += (targetLeft - currentLeft) * 0.1; // Adjust the easing factor (0.1) to make it smoother or sharper
+    currentLeft += (targetLeft - currentLeft) * 0.1;
     adjustImagePositions(currentLeft);
     requestAnimationFrame(animate);
   }
+
+  window.addEventListener("resize", function() {
+    initialPosition = updateInitialPosition();
+    targetLeft = initialPosition;
+    currentLeft = initialPosition;
+  });
 
   window.addEventListener("scroll", handleScroll);
   requestAnimationFrame(animate);
