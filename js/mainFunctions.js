6c3e694dc3b73f3f2fa5ee93bd191a2ef6ad6f5a
Ron Ward <defroged@gmail.com>
2024-06-03 20:25:58 +0900

    rollback

--- js/mainFunctions.js
+++ js/mainFunctions.js
@@ -1,14 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-  function setViewportHeight() {
-    const vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  }
-
-  setViewportHeight();
-
-  window.addEventListener('resize', setViewportHeight);
-});
-
 // Toggle class menu
 $(function () {
     $('.menu').on('click', function () {
